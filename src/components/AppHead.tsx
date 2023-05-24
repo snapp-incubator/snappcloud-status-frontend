22bc6c28eda45944aac525919a2b4e40d8aa15cb
mohammadne <mohammadne.dev@gmail.com>
2023-05-24 11:01:56 +0330

    feat: reorganize application

---
+++ src/components/AppHead.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const AppHead: React.FC = () => {
+    return (
+        <Head>
+            <title>Snappcloud Status Page</title>
+            <meta name="author" content="Snapp" />
+            <meta name="description" content="Snappcloud status page for demonstrating" />
+        </Head>
+    );
+};
+
+export default AppHead;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+import AppHead from "src/components/AppHead";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <AppHead />
+        </>
+    );
+}
+
+export default Home;
