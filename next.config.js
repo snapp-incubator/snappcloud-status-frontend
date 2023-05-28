3bfa910e858081a0a86930efb736ebc968018f35
mohammadne <mohammadne.dev@gmail.com>
2023-05-28 14:53:44 +0330

    feat: update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const is_production = process.env.NODE_ENV === 'production';
-const backend_url = is_production ? "snappcloud-status-backend-server" : "http://127.0.0.1:8080";
+const backend_url = is_production ? "http://snappcloud-status-backend-server" : "http://127.0.0.1:8080";
 
 module.exports = {
     env: {
