0fc3343beb4d9b19c6ac2d5a6c5d45a39f0b11d2
mohammadne <mohammadne.dev@gmail.com>
2023-06-23 05:26:09 +0330

    feat: update routes

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ const is_production = process.env.NODE_ENV === 'production';
 module.exports = {
     env: {
         TEH1_BACKEND_URL: is_production ?
-            "http://snappcloud-status-backend.snappcloud-monitoring.apps.private.okd4.teh-1.snappcloud.io" :
+            "http://spcld-status-backend-snappcloud-status-backend.snappcloud-monitoring.apps.private.okd4.teh-1.snappcloud.io" :
             "http://127.0.0.1:8080",
         TEH2_BACKEND_URL: is_production ?
-            "http://snappcloud-status-backend.snappcloud-monitoring.apps.private.okd4.teh-2.snappcloud.io" :
+            "http://spcld-status-backend-snappcloud-status-backend.snappcloud-monitoring.apps.private.okd4.teh-2.snappcloud.io" :
             "http://127.0.0.1:8080",
         SNAPPGROUP_BACKEND_URL: is_production ?
-            "http://snappcloud-status-backend.snappcloud-monitoring.apps.private.snappgroup.teh-1.snappcloud.io" :
+            "http://spcld-status-backend-snappcloud-status-backend.snappcloud-monitoring.apps.private.snappgroup.teh-1.snappcloud.io" :
             "http://127.0.0.1:8080",
     },
     output: 'standalone',
