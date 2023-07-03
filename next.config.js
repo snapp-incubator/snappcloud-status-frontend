cab8f780c6e10621d3bae21ddb97b966774a2810
Mohammad Yosefpor <47300215+m-yosefpor@users.noreply.github.com>
2023-07-03 11:36:05 +0330

    fix: change scheme to https

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ const is_production = process.env.NODE_ENV === 'production';
 module.exports = {
     env: {
         TEH1_BACKEND_URL: is_production ?
-            "http://status-be.apps.private.okd4.teh-1.snappcloud.io" :
+            "https://status-be.apps.private.okd4.teh-1.snappcloud.io" :
             "http://127.0.0.1:8080",
         TEH2_BACKEND_URL: is_production ?
-            "http://status-be.apps.private.okd4.teh-2.snappcloud.io" :
+            "https://status-be.apps.private.okd4.teh-2.snappcloud.io" :
             "http://127.0.0.1:8080",
         SNAPPGROUP_BACKEND_URL: is_production ?
-            "http://status-be.apps.private.snappgroup.teh-1.snappcloud.io" :
+            "https://status-be.apps.private.snappgroup.teh-1.snappcloud.io" :
             "http://127.0.0.1:8080",
     },
     output: 'standalone',
