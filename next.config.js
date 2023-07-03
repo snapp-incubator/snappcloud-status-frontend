c26d670c3cf1085059e322b6984e46f86ea4deae
Meysam Jafari <meysam.jeffrey@gmail.com>
2023-07-03 13:08:49 +0330

    Added .env file and improve readme file

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 
-const is_production = process.env.NODE_ENV === 'production';
+require('dotenv').config();
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    env: {
-        TEH1_BACKEND_URL: is_production ?
-            "http://spcld-status-backend-snappcloud-status-backend.snappcloud-monitoring.apps.private.okd4.teh-1.snappcloud.io" :
-            "http://127.0.0.1:8080",
-        TEH2_BACKEND_URL: is_production ?
-            "http://spcld-status-backend-snappcloud-status-backend.snappcloud-monitoring.apps.private.okd4.teh-2.snappcloud.io" :
-            "http://127.0.0.1:8080",
-        SNAPPGROUP_BACKEND_URL: is_production ?
-            "http://spcld-status-backend-snappcloud-status-backend.snappcloud-monitoring.apps.private.snappgroup.teh-1.snappcloud.io" :
-            "http://127.0.0.1:8080",
-    },
-    output: 'standalone',
+  env: {
+    TEH1_BACKEND_URL: isProduction ? '' : process.env.TEH1_BACKEND_URL,
+    TEH2_BACKEND_URL: isProduction ? '' : process.env.TEH2_BACKEND_URL,
+    SNAPPGROUP_BACKEND_URL: isProduction ? '' : process.env.SNAPPGROUP_BACKEND_URL,
+  },
+  output: 'standalone',
 };
+
+
+
