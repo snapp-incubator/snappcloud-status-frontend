6f1fd09e104ea744b1375e9a86eac417f7fc81ee
Meysam Jafari <meysam.jeffrey@gmail.com>
2023-07-05 11:36:24 +0330

    Fix minor bug in the production state

--- next.config.js
+++ next.config.js
@@ -6,9 +6,9 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   env: {
-    TEH1_BACKEND_URL: isProduction ? '' : process.env.TEH1_BACKEND_URL,
-    TEH2_BACKEND_URL: isProduction ? '' : process.env.TEH2_BACKEND_URL,
-    SNAPPGROUP_BACKEND_URL: isProduction ? '' : process.env.SNAPPGROUP_BACKEND_URL,
+    TEH1_BACKEND_URL: isProduction ? process.env.TEH1_BACKEND_URL : '' ,
+    TEH2_BACKEND_URL: isProduction ? process.env.TEH2_BACKEND_URL : '',
+    SNAPPGROUP_BACKEND_URL: isProduction ? process.env.SNAPPGROUP_BACKEND_URL : '',
   },
   output: 'standalone',
 };
